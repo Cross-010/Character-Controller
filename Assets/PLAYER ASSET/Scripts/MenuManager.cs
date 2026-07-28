2517d6f8f24e4d3f86196875117d612f55c0222d
Cross-010 <dionroman093@gmail.com>
2026-07-28 23:34:15 +0800

    More Update!

--- Assets/PLAYER ASSET/Scripts/MenuManager.cs
+++ Assets/PLAYER ASSET/Scripts/MenuManager.cs
@@ -3,11 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public bool gameEnded = false;
+    public GameObject Victory;
+
+    void Start()
+    {
+        if (Victory != null)
+        {
+            Victory.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            WinGame();
+        }
+    }
+
     public void NewGame()
     {
         Debug.Log("New Game Start!");
         SceneManager.LoadScene("Gameplay");
     }
+
     public void Quit()
     {
         Debug.Log("Quit!");
@@ -25,19 +45,37 @@ public class MenuManager : MonoBehaviour
         Debug.Log("Back to Main Menu!");
         SceneManager.LoadScene("Main Menu");
     }
+
     public void Credits()
     {
         Debug.Log("Credits Time!");
         SceneManager.LoadScene("Credits");
     }
+
     public void Tutorial()
     {
         Debug.Log("Tutorial!");
         SceneManager.LoadScene("Tutorial");
     }
+
     public void FightTutorial()
     {
-        Debug.Log("Tutorial!");
+        Debug.Log("Fight Tutorial!");
         SceneManager.LoadScene("FightTutorial");
+    }
+
+    public void WinGame()
+    {
+        gameEnded = true;
+
+        if (Victory != null)
+        {
+            Victory.SetActive(true);
+            Debug.Log("Victory screen is shown.");
+        }
+        else
+        {
+            Debug.LogWarning("Victory GameObject is not assigned in the Inspector!");
+        }
     }
 }
